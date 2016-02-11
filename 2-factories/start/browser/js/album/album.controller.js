ac3dcff8cd4927f32f6c9be3698143222f44f8f5
leilabl <leilaloezer@gmail.com>
2016-02-11 14:26:50 -0500

    progress on factory creation

--- 2-factories/start/browser/js/album/album.controller.js
+++ 2-factories/start/browser/js/album/album.controller.js
@@ -1,20 +1,25 @@
 'use strict';
 
-juke.controller('AlbumCtrl', function($scope, $http, $rootScope, $log) {
+juke.controller('AlbumCtrl', function($scope, $http, $rootScope, $log, StatsFactory) {
 
-  // load our initial data
-  $http.get('/api/albums/')
-  .then(res => $http.get('/api/albums/' + res.data[1]._id)) // temp: use first
-  .then(res => res.data)
-  .then(album => {
-    album.imageUrl = '/api/albums/' + album._id + '.image';
-    album.songs.forEach(function (song, i) {
-      song.audioUrl = '/api/songs/' + song._id + '.audio';
-      song.albumIndex = i;
-    });
-    $scope.album = album;
-  })
-  .catch($log.error); // $log service can be turned on and off; also, pre-bound
+  // // load our initial data
+  // $http.get('/api/albums/')
+  // .then(res => $http.get('/api/albums/' + res.data[1]._id)) // temp: use first
+  // .then(res => res.data)
+  // .then(album => {
+  //   album.imageUrl = '/api/albums/' + album._id + '.image';
+  //   album.songs.forEach(function (song, i) {
+  //     song.audioUrl = '/api/songs/' + song._id + '.audio';
+  //     song.albumIndex = i;
+  //   });
+  //   $scope.album = album;
+  //   StatsFactory.totalTime(album)
+  //   .then(function (albumDuration)  {
+  //     $scope.fullDuration = albumDuration;
+  //   })
+  // })
+
+  // .catch($log.error); // $log service can be turned on and off; also, pre-bound
 
   // main toggle
   $scope.toggle = function (song) {
@@ -39,17 +44,38 @@ juke.controller('AlbumCtrl', function($scope, $http, $rootScope, $log) {
   };
 
   // a "true" modulo that wraps negative to the top of the range
-  function mod (num, m) { return ((num % m) + m) % m; };
+  // function mod (num, m) { return ((num % m) + m) % m; };
 
   // jump `interval` spots in album (negative to go back, default +1)
-  function skip (interval) {
-    if (!$scope.currentSong) return;
-    var index = $scope.currentSong.albumIndex;
-    index = mod( (index + (interval || 1)), $scope.album.songs.length );
-    $scope.currentSong = $scope.album.songs[index];
-    if ($scope.playing) $rootScope.$broadcast('play', $scope.currentSong);
-  };
-  function next () { skip(1); };
-  function prev () { skip(-1); };
+  // function skip (interval) {
+  //   if (!$scope.currentSong) return;
+  //   var index = $scope.currentSong.albumIndex;
+  //   index = mod( (index + (interval || 1)), $scope.album.songs.length );
+  //   $scope.currentSong = $scope.album.songs[index];
+  //   if ($scope.playing) $rootScope.$broadcast('play', $scope.currentSong);
+  // };
+  // function next () { skip(1); };
+  // function prev () { skip(-1); };
 
 });
+
+// juke.factory('StatsFactory', function ($q) {
+//     var statsObj = {};
+//     statsObj.totalTime = function (album) {
+//         var audio = document.createElement('audio');
+//         return $q(function (resolve, reject) {
+//             var sum = 0;
+//             var n = 0;
+//             function resolveOrRecur () {
+//                 if (n >= album.songs.length) resolve(sum);
+//                 else audio.src = album.songs[n++].audioUrl;
+//             }
+//             audio.addEventListener('loadedmetadata', function () {
+//                 sum += audio.duration;
+//                 resolveOrRecur();
+//             });
+//             resolveOrRecur();
+//         });
+//     };
+//     return statsObj;
+// });
--- 2-factories/start/browser/js/player/player.factory.js
+++ 2-factories/start/browser/js/player/player.factory.js
@@ -1,5 +1,108 @@
 'use strict';
 
-juke.factory('PlayerFactory', function(){
+juke.factory('PlayerFactory', function ($http, $log){
   // non-UI logic in here
+  var currentSong;
+  var playing = false;
+  var nextSong, prevSong;
+
+  var currAlbum;
+  var album = {};
+
+    // initialize audio player (note this kind of DOM stuff is odd for Angular)
+  var audio = document.createElement('audio');
+  // audio.addEventListener('ended', $scope.next);
+  // audio.addEventListener('timeupdate', function () {
+  //   $scope.progress = 100 * audio.currentTime / audio.duration;
+  //   $scope.$digest(); // no Angular-aware code is doing this for us here
+  // });
+
+  // load our initial data
+  $http.get('/api/albums/')
+  .then(res => $http.get('/api/albums/' + res.data[0]._id)) // temp: use first
+  .then(res =>res.data)
+  .then(album => {
+  	// console.log(album)
+    album.imageUrl = '/api/albums/' + album._id + '.image';
+    album.songs.forEach(function (song, i) {
+      song.audioUrl = '/api/songs/' + song._id + '.audio';
+      song.songIndex = i;
+    });
+    currAlbum = album;
+    // StatsFactory.totalTime(album)
+  })
+
+  .catch($log.error); // $log service can be turned on and off; also, pre-bound
+
+   function mod (num, m) { return ((num % m) + m) % m; };
+
+
+
+
+
+  return {
+
+  	start: function (song, songList){
+    // stop existing audio (e.g. other song) in any case
+	    this.pause();
+	    playing = true;
+	    if (song === currentSong) return audio.play();
+	    // enable loading new song
+	    currentSong = song;
+	    audio.src = song.audioUrl;
+	    audio.load();
+	    audio.play();
+
+	 },
+
+	pause: function () {
+	    audio.pause();
+	    playing = false;
+	},
+
+	resume: function (song) {
+		// resume current song
+	    if (song === currentSong) return audio.play();
+	},
+
+	isPlaying: function () {
+		if (!playing) {
+			this.start(currentSong);
+			return false;
+		} else {
+			this.pause();
+			return true;
+		}
+	},
+
+	getCurrentSong: function () {
+		// console.log(currentSong)
+		if (currentSong) return currentSong;
+		return null;
+	},
+	// },
+
+	skip: function (interval) {
+		// console.log(currAlbum)
+		if (!currentSong) return;
+    var index = currentSong.songIndex;
+    index = mod( (index + (interval || 1)), currAlbum.songs.length );
+    currentSong = currAlbum.songs[index];
+    if (playing) this.start(currentSong);
+	},
+
+	next: function () {
+		this.skip(1);
+	},
+
+	previous: function () {
+		this.skip(-1);
+
+	}
+
+
+  	
+  };
 });
+
+
